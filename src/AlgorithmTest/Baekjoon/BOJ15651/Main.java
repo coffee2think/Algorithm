e1d472787c572f8de7743c31aad27587f4e11800
coffee2think <mathematicy@gmail.com>
2023-06-10 01:31:56 +0900

    230610 BOJ15651 solved

---
+++ src/AlgorithmTest/Baekjoon/BOJ15651/Main.java
@@ -0,0 +1,39 @@
+package AlgorithmTest.Baekjoon.BOJ15651;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr; // arr를 전역변수로 선언하는 것이 시간을 줄이는데 큰 영향을 줬음
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        backtrack(0);
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    public static void backtrack(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            backtrack(depth + 1);
+        }
+    }
+}
