2ba4ee56ac35cd9128037bb7d70af6d4abe62bf2
coffee2think <135593667+coffee2think@users.noreply.github.com>
2023-06-20 22:41:50 +0900

    230620 BOJ9663 solved

--- src/AlgorithmTest/Baekjoon/BOJ9663/Main.java
+++ src/AlgorithmTest/Baekjoon/BOJ9663/Main.java
@@ -3,93 +3,41 @@ package AlgorithmTest.Baekjoon.BOJ9663;
 import java.io.*;
 
 public class Main {
-    static int count = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        System.out.println(NQueen(N));
+        System.out.println(nQueen(N));
 
         br.close();
     }
 
-    public static int NQueen(int n) {
-        int[][] board = new int[n][n];
-        return NQueen(board, 0, 0);
+    public static int nQueen(int n) {
+        int[] board = new int[n];
+        return nQueen(board, 0, 0);
     }
 
-    private static int NQueen(int[][] board, int row, int cnt) {
-        int boardSize = board.length;
-        System.out.println("실행 횟수 : " + ++count);
-
-        if (row == boardSize) {
+    private static int nQueen(int[] board, int depth, int cnt) {
+        if (depth == board.length) {
             return cnt + 1;
         }
 
-        for (int x = 0; x < boardSize; x++) {
-            if (!checkOverlap(board, row, x)) {
-                board[row][x] = true;
-                cnt = NQueen(board, row + 1, cnt);
-                board[row][x] = false;
+        for (int i = 1; i <= board.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < depth; j++) {
+                if (board[j] == i || board[j] == i + (depth - j) || board[j] == i - (depth - j)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                board[depth] = i;
+                cnt = nQueen(board, depth + 1, cnt);
+                board[depth] = 0;
             }
         }
 
         return cnt;
-    }
-
-    public static boolean checkOverlap(boolean[][] board, int row, int col) {
-        int[] dx = {1, -1, -1, 1};
-        int[] dy = {1, 1, -1, -1};
-        int boardSize = board.length;
-
-        // 수직, 수평 검사
-        for (int i = 0; i < boardSize; i++) {
-            if (board[row][i] || board[i][col]) {
-                return true;
-            }
-        }
-        // 대각선 검사
-        for (int i = 0; i < 4; i++) {
-            int x = col + dx[i];
-            int y = row + dy[i];
-            while (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-                if (board[y][x]) {
-                    return true;
-                } else {
-                    x += dx[i];
-                    y += dy[i];
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public static void fillBoard(int[][] board, int row, int col, int num){
-        int[] dx = {1, -1, -1, 1};
-        int[] dy = {1, 1, -1, -1};
-        int boardSize = board.length;
-
-        if(row == boardSize){
-            return;
-        }
-
-        for(int i=0; i<boardSize; i++){
-            if(board[row][i] == 0) board[row][i] = num;
-            if(board[i][col] == 0) board[i][col] = num;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int x = col + dx[i];
-            int y = row + dy[i];
-            while (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-                if (board[y][x] == 0) {
-                    board[y][x] = num;
-                }
-                x += dx[i];
-                y += dy[i];
-            }
-        }
     }
 }
