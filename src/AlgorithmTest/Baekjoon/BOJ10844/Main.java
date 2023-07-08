8f4e244a5ddc06930e4b354a6edd01602e939001
coffee2think <135593667+coffee2think@users.noreply.github.com>
2023-07-08 21:45:04 +0900

    230708 BOJ10844 solved!

---
+++ src/AlgorithmTest/Baekjoon/BOJ10844/Main.java
@@ -0,0 +1,32 @@
+package AlgorithmTest.Baekjoon.BOJ10844;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] stairNum = new int[N][10];
+        for (int i = 1; i < 10; i++) {
+            stairNum[0][i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            stairNum[i][0] = stairNum[i - 1][1];
+            stairNum[i][9] = stairNum[i - 1][8];
+            for (int n = 1; n < 9; n++) {
+                stairNum[i][n] = (stairNum[i - 1][n - 1] + stairNum[i - 1][n + 1]) % 1000000000;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 10; i++) {
+            result = (result + stairNum[N - 1][i]) % 1000000000;
+        }
+        System.out.println(result);
+
+        br.close();
+    }
+}
