13a1c5690c155a4aa0e252f3c49043ebcacf3a3d
coffee2think <135593667+coffee2think@users.noreply.github.com>
2023-07-09 14:12:05 +0900

    230709 BOJ2156 solved

--- src/AlgorithmTest/Baekjoon/BOJ2156/Main.java
+++ src/AlgorithmTest/Baekjoon/BOJ2156/Main.java
@@ -1,26 +1,31 @@
-package BOJ2156;
+package Baekjoon.BOJ2156;
 
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		int[] wine = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			wine[i] = Integer.parseInt(br.readLine());
-		}
+        int[] wine = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
 
-		int[] manyWine = new int[wine.length];
-		manyWine[1] = wine[1];
-		if (n > 1) manyWine[2] = manyWine[1] + wine[2];
-		for (int i = 3; i < wine.length; i++) {
-			manyWine[i] = Math.max(manyWine[i - 2], manyWine[i - 3] + wine[i - 1]) + wine[i];
-		}
+        int[] dp = new int[wine.length];
+        dp[1] = wine[1];
+        if (n > 1) dp[2] = dp[1] + wine[2];
+        for (int i = 3; i < wine.length; i++) {
+            dp[i] = Math.max(Math.max(dp[i - 2], dp[i - 3] + wine[i - 1]) + wine[i], dp[i - 1]);
+            /* 선택할 수 있는 3가지 경우
+             * i-3에서 점프하여 i-1을 포함하고, i를 포함하는 경우
+             * i-2에서 점프하고 i를 포함하는 경우
+             * i-1까지의 최댓값에 i를 포함하지 않는 경우
+             */
+        }
 
-		System.out.println(Math.max(manyWine[n - 1], manyWine[n]));
-		br.close();
-	}
+        System.out.println(dp[n]);
+        br.close();
+    }
 }
