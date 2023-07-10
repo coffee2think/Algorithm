5d64a1a415aae573ea404c8de52f3f41686fd718
coffee2think <135593667+coffee2think@users.noreply.github.com>
2023-07-10 22:27:47 +0900

    230709 BOJ11053 solved!

---
+++ src/AlgorithmTest/Baekjoon/BOJ11053/Main.java
@@ -0,0 +1,39 @@
+package AlgorithmTest.Baekjoon.BOJ11053;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] seq = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < seq.length; i++){
+            dp[i] = 1;
+
+            for(int j = 0; j < i; j++){
+                if(seq[j] < seq[i] && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = -1;
+        for(int i = 0; i < dp.length; i++){
+            if(dp[i] > max){
+                max = dp[i];
+            }
+        }
+
+        System.out.println(max);
+        br.close();
+    }
+}
