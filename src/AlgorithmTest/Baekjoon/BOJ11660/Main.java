6e8e60dc2f7f0d51fbfa2dd17f823187cb463afd
coffee2think <135593667+coffee2think@users.noreply.github.com>
2023-07-23 14:27:07 +0900

    230723 BOJ11660 solved

---
+++ src/AlgorithmTest/Baekjoon/BOJ11660/Main.java
@@ -0,0 +1,38 @@
+package AlgorithmTest.Baekjoon.BOJ11660;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] psum = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                psum[i][j] = psum[i][j-1] + psum[i-1][j] - psum[i-1][j-1];
+                psum[i][j] += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int x1, y1, x2, y2;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(psum[x2][y2] - psum[x2][y1 - 1] - psum[x1 - 1][y2] + psum[x1 - 1][y1 - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
