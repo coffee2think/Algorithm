33cb79dee918625b3fa27478987c1690a2bf51ca
coffee2think <mathematicy@gmail.com>
2024-04-08 00:53:24 +0900

    programmers Q68644 performance check

--- src/programmers/lv1/Q68644/Main.java
+++ src/programmers/lv1/Q68644/Main.java
@@ -1,18 +1,33 @@
 package src.programmers.lv1.Q68644;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2,1,3,4,1};
-        int[] result = solution(numbers);
+//        int[] numbers = {2,1,3,4,1};
+        int arraySize = 10000;
+        int[] numbers = new int[arraySize];
+        Random random = new Random();
+        for (int i = 0; i < arraySize; i++) {
+            numbers[i] = random.nextInt(101);
+        }
 
-        System.out.println(Arrays.toString(result));
+        long start1 = System.nanoTime();
+        int[] result1 = solution1(numbers);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        int[] result2 = solution2(numbers);
+        long end2 = System.nanoTime();
+
+        System.out.println("result1 time : " + ((end1 - start1) / 1000000) + " ms");
+        System.out.println("result2 time : " + ((end2 - start2) / 1000000) + " ms");
     }
 
-    public static int[] solution(int[] numbers) {
+    public static int[] solution1(int[] numbers) {
         Set<Integer> set = new TreeSet<>();
 
         for (int i = 0; i < numbers.length - 1; i++) {
@@ -24,5 +39,23 @@ public class Main {
         return set.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
+    }
+
+    public static int[] solution2(int[] numbers) {
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        Integer[] result = set.toArray(new Integer[0]);
+        int[] answer = new int[result.length];
+        for (int i = 0; i < result.length; i++) {
+            answer[i] = result[i];
+        }
+
+        return answer;
     }
 }
