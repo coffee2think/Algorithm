b0819d24572f4224c28c09fd426f60a537ff728c
coffee2think <mathematicy@gmail.com>
2024-04-22 23:36:19 +0900

    programmers Q42747 H-Index

--- src/programmers/lv2/Q42747/Main.java
+++ src/programmers/lv2/Q42747/Main.java
@@ -1,6 +1,12 @@
-package programmers.lv2.hindex;
+package src.programmers.lv2.Q42747;
 
 import java.util.Arrays;
+
+/**
+ * Programmers Q42747 lv2
+ * Problem name: H-Index
+ * link: https://school.programmers.co.kr/learn/courses/30/lessons/42747
+ */
 
 public class Main {
     public static void main(String[] args) {
