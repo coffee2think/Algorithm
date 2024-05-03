4873751e62717d3e182bb842436e30a580b25959
coffee2think <mathematicy@gmail.com>
2024-05-03 14:59:34 +0900

    baekjoon #15700

---
+++ src/baekjoon/BOJ15700/Main.java
@@ -0,0 +1,24 @@
+package src.baekjoon.BOJ15700;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * date: 2024-05-03
+ * source: https://www.acmicpc.net/problem/15700
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println((long) N * M / 2);
+        br.close();
+    }
+}
