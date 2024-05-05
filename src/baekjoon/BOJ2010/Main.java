7015ffccf096c0414daf3000665ac53d78252b97
coffee2think <mathematicy@gmail.com>
2024-05-05 17:13:51 +0900

    baekjoon #2010

---
+++ src/baekjoon/BOJ2010/Main.java
@@ -0,0 +1,26 @@
+package src.baekjoon.BOJ2010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * date: 2024-05-05
+ * source: https://www.acmicpc.net/problem/2010
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum - (N - 1));
+        br.close();
+    }
+}
