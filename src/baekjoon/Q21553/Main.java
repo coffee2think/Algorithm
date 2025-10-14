897bad1c83360f4d85798f8fcf6ce68c05296aa2
coffee2think <mathematicy@gmail.com>
2025-10-14 23:43:50 +0900

    baekjoon #21553

---
+++ src/baekjoon/Q21553/Main.java
@@ -0,0 +1,17 @@
+package src.baekjoon.Q21553;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// link: https://www.acmicpc.net/problem/21553
+// date: 2025-10-14
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String P = br.readLine();
+        System.out.println(P);
+        br.close();
+    }
+}
